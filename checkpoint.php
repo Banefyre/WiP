8aa8513d647269c5915d0eedc78a9af5a34f4cea
Melodie SAGODIRA <msagodir@e2r9p1.42.fr>
2014-05-25 00:41:42 +0200

    checkpoint.php

---
+++ checkpoint.php
@@ -0,0 +1,15 @@
+<?php
+
+include('php/header.php');
+include('php/menu.php');
+
+$id_timeline = 1;
+
+$mysql = connect();
+$res = $mysql->query("SELECT * FROM checkpoint WHERE id_timeline = ".$id_timeline);
+$res = $res->fetch_array();
+
+
+$date = date_create_from_format("Y-m-d\TH:i:sO", $date);
+    $begin = date_create_from_format("Y-m-d\TH:i:sO", $begin);
+?>
